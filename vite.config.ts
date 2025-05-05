6aa8fdac431dfe1acf14f29b684f3f0b6eeeb095
lzy <3312202467@qq.com>
2025-05-05 17:54:32 +0800

    fix:部署配置修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,38 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
-// https://vite.dev/config/
+// 判断是否构建阶段
+const isBuild = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
     sourcemap: true,
     emptyOutDir: true,
+    rollupOptions: {
+      plugins: isBuild ? [rollupNodePolyFill()] : [],
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis', // 修复 global is not defined
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {

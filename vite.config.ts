1500b38f9dff9395b02f4cc0ccb7a13d6ddf8a35
lzy <3312202467@qq.com>
2025-05-05 18:00:30 +0800

    fix:部署配置修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
-// 判断是否构建阶段
-const isBuild = process.env.NODE_ENV === 'production'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  plugins: [vue(), vueJsx()],
   build: {
     outDir: 'dist',
     sourcemap: true,
     emptyOutDir: true,
-    rollupOptions: {
-      plugins: isBuild ? [rollupNodePolyFill()] : [],
-    },
+    // rollupOptions: {
+    //   input: ['src/index.html']
+    // }
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis', // 修复 global is not defined
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
